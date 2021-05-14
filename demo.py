134e339dc103d8a606db07dfa7d3840b1393ba0b
Gnabriel <ls6l9g7g9ud1@opayq.com>
2021-05-14 12:48:36 +0200

    Updated demo

--- demo.py
+++ demo.py
@@ -24,50 +24,51 @@ def demo():
 
     out_ab = model(l_tens).cpu()
     out_lab, out_rgb = postprocess_tens(l_tens, out_ab)
-
+    print(out_ab[:, :, 0])
+    print(out_ab[:, :, 1])
     # Original image rgb2lab2rgb
-    # a_tens = preprocess_demo_image(ab_image[:, :, 0])
-    # b_tens = preprocess_demo_image(ab_image[:, :, 1])
-    # ab_tens = torch.cat((a_tens, b_tens), dim=1)
-    # original_lab, original_rgb = postprocess_tens(l_tens, ab_tens)
+    a_tens = preprocess_demo_image(ab_image[:, :, 0])
+    b_tens = preprocess_demo_image(ab_image[:, :, 1])
+    ab_tens = torch.cat((a_tens, b_tens), dim=1)
+    original_lab, original_rgb = postprocess_tens(l_tens, ab_tens)
 
     # Save the colored image.
     plt.imsave('out_img.png', out_rgb)
 
-    # plt.imshow(image)
-    # plt.title('Original')
+    plt.imshow(image)
+    plt.title('Original')
+    plt.axis('off')
+    plt.show()
+
+    plt.imshow(l_image, cmap='gray')
+    plt.title('Original (L channel)')
+    plt.axis('off')
+    plt.show()
+
+    # plt.imshow(out_lab[:, :, 0], cmap='gray')
+    # plt.title('Out channel 0')
     # plt.axis('off')
     # plt.show()
     #
-    # plt.imshow(l_image, cmap='gray')
-    # plt.title('Original (L channel)')
+    # plt.imshow(out_lab[:, :, 1])
+    # plt.title('Out channel 1')
     # plt.axis('off')
     # plt.show()
-
-    plt.imshow(out_lab[:, :, 0], cmap='gray')
-    plt.title('Out channel 0')
-    plt.axis('off')
-    plt.show()
-
-    plt.imshow(out_lab[:, :, 1])
-    plt.title('Out channel 1')
-    plt.axis('off')
-    plt.show()
-
-    plt.imshow(out_lab[:, :, 2])
-    plt.title('Out channel 2')
-    plt.axis('off')
-    plt.show()
+    #
+    # plt.imshow(out_lab[:, :, 2])
+    # plt.title('Out channel 2')
+    # plt.axis('off')
+    # plt.show()
 
     plt.imshow(out_rgb)
     plt.title('Output (perfectly AI colored RGB)')
     plt.axis('off')
     plt.show()
 
-    # plt.imshow(original_rgb)
-    # plt.title('Original rgb2lab2rgb')
-    # plt.axis('off')
-    # plt.show()
+    plt.imshow(original_rgb)
+    plt.title('Original rgb2lab2rgb')
+    plt.axis('off')
+    plt.show()
 
 
 if __name__ == '__main__':
