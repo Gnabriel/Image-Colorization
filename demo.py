e3331378f5d7ac123f884d9965983f6d18b31af8
Gnabriel <elleb@kth.se>
2021-06-01 10:29:33 +0200

    Added testing functions

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from scipy import ndimage
 
 from main import *
@@ -5,7 +6,7 @@ from main import *
 
 def get_trained_CNN(data_size):
     model = ColorizationNet()
-    model.load_state_dict(torch.load('./colorize_cnn_{}.pth'.format(data_size)))
+    model.load_state_dict(torch.load('./colorize_cnn_{}_bra_skit.pth'.format(data_size)))
     # model.load_state_dict(torch.load('./colorize_cnn_{}_bra_skit.pth'.format(data_size)))
     model.eval()
     print("Loaded CNN: colorize_cnn_{}".format(data_size))
@@ -103,7 +104,7 @@ def plot_q_probabilities(q1, q2):
     plt.show()
 
 
-def plot_ab_channels(a, b, file_name, save_image):
+def plot_ab_channels(a, b, path, save_image):
     fig, axes = plt.subplots(ncols=2, figsize=(12, 4))
     data = [('A', a), ('B', b)]
     for ax, (title, img) in zip(axes, data):
@@ -112,11 +113,11 @@ def plot_ab_channels(a, b, file_name, save_image):
         ax.axis('off')
     fig.tight_layout()
     if save_image:
-        plt.savefig(file_name)
+        plt.savefig(path)
     plt.show()
 
 
-def plot_images(original_rgb, output_rgb, T, file_name, save_image):
+def plot_images(original_rgb, output_rgb, T, path, save_image):
     fig, axes = plt.subplots(ncols=2, figsize=(12, 4))
     data = [('Original', original_rgb), ('Result (T={})'.format(T), output_rgb)]
     for ax, (title, img) in zip(axes, data):
@@ -125,27 +126,24 @@ def plot_images(original_rgb, output_rgb, T, file_name, save_image):
         ax.axis('off')
     fig.tight_layout()
     if save_image:
-        plt.savefig(file_name)
+        plt.savefig(path)
     plt.show()
 
 
-def demo():
-    # Parameters.
-    data_size = 100           # 13 233
-    T = 0.5
-    save_images = False
+def plot_losses(data_size, path, save_images):
+    losses = pickle.load(open("pickles/losses_{}_mse.p".format(data_size), "rb"))
+    losses = np.array(losses).flatten()
+    plt.plot(losses)
+    plt.ylabel("Loss")
+    plt.xlabel("Update step")
+    if save_images:
+        plt.savefig(path)
+    else:
+        plt.show()
 
-    # Load trained and saved CNN model.
-    model = get_trained_CNN(data_size)
 
-    # Load and pre-process demo image.
-    # original_image = np.asarray(Image.open(f"./data/Vladimir_Putin_0033.jpg"))
-    # original_image = np.asarray(Image.open(f"./data/Abdoulaye_Wade_0002.jpg"))
-    original_image = np.asarray(Image.open(f"./data/Mary_Elizabeth_Mastrantonio_0001.jpg"))
-    # original_image = np.asarray(Image.open(f"./data/Pascal_Affi_Nguessan_0001.jpg"))
-    # original_image = np.asarray(Image.open(f"./data/Pascal_Quignard_0003.jpg"))
-    l_original, ab_original = preprocess_image(original_image)
-    l_original_tens = image_to_tens(l_original)
+def experiment(data_size, model, num_images, T=0.6):
+    _, test_X, _, test_Y = pickle.load(open("pickles/train_test_{}.p".format(data_size), "rb"))
 
     # Load and get helpers.
     ab_to_q_dict_unsorted = pickle.load(open("pickles/ab_to_q_index_dict_unsorted.p", "rb"))
@@ -153,23 +151,89 @@ def demo():
     ab_to_q_dict = get_ab_to_q_dict(data_size, ab_domain)
     q_to_ab_dict = get_q_to_ab_dict(data_size, ab_to_q_dict)
 
-    # Get prediction for demo image by CNN.
-    Z_output_tens = model(l_original_tens).cpu()
+    for i in range(num_images):
+        l_original, ab_original = test_X[i], test_Y[i]
+        l_original_tens = image_to_tens(l_original)
 
-    # Post-process prediction.
-    Z_output, lab_output, rgb_output = postprocess_output(l_original, Z_output_tens, ab_domain, q_to_ab_dict, T)
+        # Get prediction for demo image by CNN.
+        Z_output_tens = model(l_original_tens).cpu()
 
-    i = 0
-    while os.path.exists("colorized_image%s.png" % i):
-        i += 1
+        # Post-process prediction.
+        Z_output, lab_output, rgb_output = postprocess_output(l_original, Z_output_tens, ab_domain, q_to_ab_dict, T)
 
-    # Plot images and color-channels.
-    plot_ab_channels(lab_output[:, :, 1], lab_output[:, :, 2], "image_results/ab_channels_%s.png" % i, save_images)
-    plot_images(original_image, rgb_output, T, "image_results/original_vs_colored_%s.png" % i, save_images)
+        # Save the grayscale image.
+        plt.imsave('experiment/original_cnn/grayscale_{}.png'.format(i+1), l_original, cmap='gray')
 
-    # Save the colored image.
-    if save_images:
-        plt.imsave('out_img.png', rgb_output)
+        # Save the colored output image.
+        plt.imsave('experiment/original_cnn/out_{}.png'.format(i+1), rgb_output)
+
+        # Save the original image.
+        lab_original = np.empty((l_original.shape[0], l_original.shape[1], 3))
+        lab_original[:, :, 0], lab_original[:, :, 1:] = l_original, ab_original
+        rgb_original = lab_to_rgb(lab_original)
+        plt.imsave('experiment/original_cnn/original_{}.png'.format(i+1), rgb_original)
+
+        print("Image {} done and saved.".format(i+1))
+
+
+def demo():
+    # Parameters.
+    data_size = 13233           # 13 233
+    # T = 0.38
+    # T_steps = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 0.38]
+    T_steps = [0.6]
+    save_images = True
+
+    # Load trained and saved CNN model.
+    model = get_trained_CNN(data_size)
+
+    # experiment(data_size, model, num_images=50)
+    # exit("Experiment done")
+
+    for T in T_steps:
+        # Load and pre-process demo image.
+        # original_image = np.asarray(Image.open(f"./data/Vladimir_Putin_0033.jpg"))
+        # original_image = np.asarray(Image.open(f"./data/Abdoulaye_Wade_0002.jpg"))
+        # original_image = np.asarray(Image.open(f"./data/Mary_Elizabeth_Mastrantonio_0001.jpg"))
+        # original_image = np.asarray(Image.open(f"./data/Pascal_Affi_Nguessan_0001.jpg"))
+        # original_image = np.asarray(Image.open(f"./data/Pascal_Quignard_0003.jpg"))
+        # original_image = np.asarray(Image.open(f"./data/Ai_Sugiyama_0004.jpg"))
+        # original_image = np.asarray(Image.open(f"./data/Saddam_Hussein_0009.jpg"))
+        original_image = np.asarray(Image.open(f"./data_imgnet/gabbe_glad.png"))[:, :, :3]
+        # original_image = np.asarray(Image.open(f"./data_imgnet/ILSVRC2012_val_00005026.jpeg"))  # lemurer
+        # original_image = np.asarray(Image.open(f"./data_imgnet/ILSVRC2012_val_00005002.jpeg"))  # hamster
+        # original_image = np.asarray(Image.open(f"./data_imgnet/ILSVRC2012_val_00005006.jpeg"))  # tiger
+        # original_image = np.asarray(Image.open(f"./data_imgnet/ILSVRC2012_val_00005011.jpeg"))  # tiger
+        # original_image = np.asarray(Image.open(f"./data_imgnet/gabe.jpg"))  # tony the tiger
+        l_original, ab_original = preprocess_image(original_image)
+        l_original_tens = image_to_tens(l_original)
+
+        # Load and get helpers.
+        ab_to_q_dict_unsorted = pickle.load(open("pickles/ab_to_q_index_dict_unsorted.p", "rb"))
+        ab_domain = get_ab_domain(data_size, ab_to_q_dict_unsorted)
+        ab_to_q_dict = get_ab_to_q_dict(data_size, ab_domain)
+        q_to_ab_dict = get_q_to_ab_dict(data_size, ab_to_q_dict)
+
+        # Get prediction for demo image by CNN.
+        Z_output_tens = model(l_original_tens).cpu()
+
+        # Post-process prediction.
+        Z_output, lab_output, rgb_output = postprocess_output(l_original, Z_output_tens, ab_domain, q_to_ab_dict, T)
+
+        # Plot images and color-channels.
+        plot_ab_channels(lab_output[:, :, 1], lab_output[:, :, 2], "image_results/ab_channels_{}.png".format(T), save_images)
+        plot_images(original_image, rgb_output, T, "image_results/original_vs_colored_{}.png".format(T), save_images)
+
+        # Plot losses.
+        # plot_losses(data_size, "image_results/losses.png", save_images)
+
+        # Save the grayscale image.
+        if save_images:
+            plt.imsave('image_results/grayscale.png', l_original, cmap='gray')
+
+        # Save the colored image.
+        if save_images:
+            plt.imsave('image_results/out_img_{}.png'.format(T), rgb_output)
 
 
 if __name__ == '__main__':
